b9ec275522e2ac078930884b302bd63d5d97a57d
Fitsumhelina <permin1501424@gmail.com>
2025-01-06 20:25:59 +0300

    new js question solved

--- Interview-Questions/js-01.js
+++ Interview-Questions/js-01.js
@@ -13,3 +13,10 @@
 
 // console.log(typeof typeof 100)
 
+// const obj = [..."hello" ]
+// console.log(obj)
+
+// console.log(parseInt("10+2"))
+// console.log(parseInt("7FM"))
+// console.log(parseInt("M7F"))
+
